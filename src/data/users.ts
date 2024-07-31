276ccdddce6c1d8719f6a0f4dc96d4a23ee35cd5
designindev <2438599@gmail.com>
2024-07-31 17:20:01 +0300

    added auth

--- src/data/users.ts
+++ src/data/users.ts
@@ -3,12 +3,12 @@ export const users = [
     id: '1',
     email: 'user1@gmail.com',
     name: 'User 1',
-    password: '12345',
+    password: '12345678',
   },
   {
-    id: '1',
+    id: '2',
     email: 'user2@gmail.com',
     name: 'User 2',
-    password: '12345',
+    password: '12345678',
   },
 ]
--- src/features/session/login/ui/LoginForm.tsx
+++ src/features/session/login/ui/LoginForm.tsx
@@ -8,8 +8,8 @@ import { LoginFormSchema, loginFormSchema } from '../model/login-form.schema'
 import { PATH_PAGE } from 'shared/lib'
 import { useLoginMutation } from 'entities/session/api/session.api'
 import Link from 'next/link'
-import { useSelector } from 'react-redux'
-import { selectIsAuth } from 'entities/session'
+import { signIn } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
 
 type Props = {
   className?: string
@@ -18,10 +18,21 @@ type Props = {
 export function LoginForm(props: Props) {
   // const [loginThunk] = useLoginMutation()
   // const isAuth = useSelector(selectIsAuth)
-  const isauth = false
+  const router = useRouter()
 
   const onSubmitHandler = useCallback(
     async (data: LoginFormSchema) => {
+      const res = await signIn('credentials', {
+        email: data.email,
+        password: data.password,
+        redirect: false,
+      })
+
+      if (res && !res.error) {
+        router.push('/admin')
+      } else {
+        console.log(res)
+      }
       // await loginThunk(data).unwrap()
       notifySuccess('You have successfully logged in')
     },

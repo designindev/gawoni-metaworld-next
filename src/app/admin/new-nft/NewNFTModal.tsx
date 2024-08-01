d692b4651683895adff74a78c20d967b2681b0df
designindev <2438599@gmail.com>
2024-08-01 11:58:22 +0300

    added 'craate new edition' modal

--- src/app/admin/new-nft/NewNFTModal.tsx
+++ src/app/admin/new-nft/NewNFTModal.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Box, Typography, Modal, TextField, FormControl, RadioGroup, FormLabel, Radio, FormControlLabel } from '@mui/material'
+import { Box, Typography, Modal, TextField, FormControl, RadioGroup, FormLabel, Radio, FormControlLabel, Checkbox, Button } from '@mui/material'
 import { COLORS } from 'shared/const'
 import CloseIcon from '@mui/icons-material/Close'
 
@@ -12,7 +12,7 @@ const styleModal = {
   display: 'flex',
   flexDirection: 'column',
   gap: 5,
-  width: 525,
+  width: 775,
   bgcolor: 'background.paper',
   border: '2px solid #000',
   boxShadow: 24,
@@ -21,6 +21,24 @@ const styleModal = {
   borderRadius: 6,
 }
 
+const colors = [
+  '#FF0000',
+  '#FF8D29',
+  '#FFEC15',
+  '#C2F51B',
+  '#62FF00',
+  '#00FFCC',
+  '#00E5FF',
+  '#009DFF',
+  '#0066FF',
+  '#0900FF',
+  '#7423FF',
+  '#B941FF',
+  '#FF41F2',
+  '#FF2160',
+  '#F80029',
+]
+
 export const NewNFTModal = (props: { open: boolean; handleClose: () => void }) => {
   const { open, handleClose } = props
 
@@ -28,19 +46,31 @@ export const NewNFTModal = (props: { open: boolean; handleClose: () => void }) =
     <>
       <Modal open={open} onClose={handleClose} aria-labelledby='modal-modal-title' aria-describedby='modal-modal-description'>
         <Box sx={styleModal}>
-          <CloseIcon onClick={handleClose} sx={{ position: 'absolute', top: '10px', right: '10px', cursor: 'pointer', fontFamily: 'Whyte' }} />
-          <Typography variant='h3' sx={{ fontSize: '26px', color: COLORS.primary }}>
+          <CloseIcon fontSize='large' onClick={handleClose} sx={{ position: 'absolute', top: '10px', right: '10px', cursor: 'pointer', fontFamily: 'Whyte' }} />
+          <Typography variant='h3' sx={{ fontSize: '26px' }}>
             Create new edition
           </Typography>
           <TextField label='Name*' placeholder='Enter edition name' />
-          <FormControl>
-            <FormLabel id='demo-row-radio-buttons-group-label'>Gender</FormLabel>
-            <RadioGroup row aria-labelledby='demo-row-radio-buttons-group-label' name='row-radio-buttons-group'>
-              <FormControlLabel value='female' control={<Radio />} label='Female' />
-              <FormControlLabel value='male' control={<Radio />} label='Male' />
-              <FormControlLabel value='other' control={<Radio />} label='Other' />
-            </RadioGroup>
-          </FormControl>
+
+          <RadioGroup defaultValue='female' name='colors' sx={{ flexDirection: 'row' }}>
+            {colors.map((color, i) => {
+              return (
+                <Radio
+                  key={i}
+                  value={color}
+                  icon={<Box width={30} height={30} sx={{ backgroundColor: color, borderRadius: 50 }}></Box>}
+                  checkedIcon={<Box width={30} height={30} sx={{ backgroundColor: color, borderRadius: 50, border: '5px solid #fff' }}></Box>}
+                  sx={{ padding: 2 }}
+                />
+              )
+            })}
+          </RadioGroup>
+          <Box display={'flex'} justifyContent={'flex-end'} gap={4}>
+            <Button color='white' variant='outlined'>
+              Cancel
+            </Button>
+            <Button>Save</Button>
+          </Box>
         </Box>
       </Modal>
     </>

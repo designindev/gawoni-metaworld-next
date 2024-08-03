57bfa6303434944b42ad7285d2c64b1ae1c68eb2
designindev <2438599@gmail.com>
2024-08-03 23:17:21 +0300

    deleted filter.scss

--- src/widgets/filter/Filter.tsx
+++ src/widgets/filter/Filter.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Box, Drawer, Button, Typography } from '@mui/material'
+import { Box, Drawer, Button, Typography, Stack } from '@mui/material'
 import classNames from 'classnames'
 import { useRef, useState } from 'react'
 import { ActionMeta, SingleValue } from 'react-select'
@@ -66,26 +66,38 @@ export const Filter = (props: Props) => {
 
   const FilterInner = (
     <>
-      <div className='filter__top'>
-        <div className='filter__left'>
+      <Stack direction={'row'} justifyContent={'space-between'} mb={7}>
+        <Stack direction={'row'}>
           <Typography variant='h4' component={'h3'}>
             Filters
           </Typography>
-          <div className='filter__number'>{props.count}</div>
-        </div>
-        <div className='filter__right'>
+          <Box
+            width={{ lg: 40, xs: 32 }}
+            height={{ lg: 40, xs: 32 }}
+            lineHeight={{ lg: '40px', xs: '32px' }}
+            bgcolor={'secondary.main'}
+            borderRadius={'50%'}
+            textAlign={'center'}
+            fontSize={18}
+            fontWeight={500}
+            ml={4}
+          >
+            {props.count}
+          </Box>
+        </Stack>
+        <Box>
           <Button color={'white'} variant={'outlined'} onClick={clearValue} endIcon={<CloseIcon />}>
             Clear all
           </Button>
-        </div>
-      </div>
+        </Box>
+      </Stack>
 
-      <div className='filter__items'>
+      <Stack direction={{ lg: 'row', xs: 'column' }} spacing={5} flexWrap={'wrap'} position={'relative'} zIndex={4}>
         {filters.map((el, i) => {
           return (
-            <div key={i} className='filter__items-item'>
-              <div className='filter__items-label'>{el.label}</div>
-              <div className='filter__items-input'>
+            <Stack direction={{ lg: 'row', xs: 'column' }} spacing={5} alignItems={'center'} key={i}>
+              <Box m={{ lg: '0 0 8px', xs: '0 0 10px' }}>{el.label}</Box>
+              <Box width={{ lg: 207, xs: '100%' }}>
                 <Select
                   onChange={props.onChange}
                   placeholder={'All'}
@@ -94,20 +106,18 @@ export const Filter = (props: Props) => {
                   inputRef={refs[i]}
                   isClearable
                 />
-              </div>
-            </div>
+              </Box>
+            </Stack>
           )
         })}
-      </div>
+      </Stack>
     </>
   )
 
+  const bgStyle = props.bg ? { padding: 6, bgColor: 'dark.main', borderRadius: 4 } : {}
+
   return (
-    <div
-      className={classNames('filter', props.className, {
-        filter__bg: props.bg,
-      })}
-    >
+    <Box {...bgStyle} mb={{ lg: 12, xs: 10 }}>
       <Box
         sx={(theme) => ({
           [theme.breakpoints.down('lg')]: {
@@ -122,20 +132,20 @@ export const Filter = (props: Props) => {
           [theme.breakpoints.up('lg')]: { display: 'none' },
         })}
       >
-        <Box className='filter__btn-open'>
+        <Box>
           <Button color={'white'} variant={'outlined'} onClick={() => setOpen(true)} fullWidth>
             Filters <div className='filter__number'>{props.count}</div>
           </Button>
         </Box>
         <Drawer anchor='bottom' open={open} onClose={() => setOpen(false)}>
           {FilterInner}
-          <div className='filter__button'>
+          <Box textAlign={'center'} mt={6}>
             <Button onClick={() => setOpen(false)} fullWidth>
               Accept
             </Button>
-          </div>
+          </Box>
         </Drawer>
       </Box>
-    </div>
+    </Box>
   )
 }

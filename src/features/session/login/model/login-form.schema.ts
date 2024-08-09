80ab6fa3fd3f0d8641949b5c2ada97d5f42bd3a6
designindev <2438599@gmail.com>
2024-08-09 12:28:07 +0300

    changed min password

--- src/features/session/login/model/login-form.schema.ts
+++ src/features/session/login/model/login-form.schema.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 
 export const loginFormSchema = z.object({
   emailOrLogin: z.string({ message: 'Email or login is required' }).min(2),
-  password: z.string({ message: 'Password is required' }).min(7),
+  password: z.string({ message: 'Password is required' }).min(8),
 })
 
 export type LoginFormSchema = z.infer<typeof loginFormSchema>

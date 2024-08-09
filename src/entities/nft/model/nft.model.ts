1a8208f45ae5ba6f3d516c5c08afb424450b3c0e
designindev <2438599@gmail.com>
2024-08-09 14:00:22 +0300

    changed color

--- src/entities/nft/model/nft.model.ts
+++ src/entities/nft/model/nft.model.ts
@@ -35,19 +35,20 @@ export const games = ['K 4 rally', 'Tuk tuk rush', 'Racing game', 'Martial arts 
 export const categories = ['Category 1', 'Category 2', 'Category 3']
 
 const mapNftColor: Record<NFT_RARITY, string> = {
-  Common: '#b3b3b3',
+  Common: '#7B8077',
   Uncommon: '#00bf13',
   Rare: '#0038ff',
   Epic: '#a700e2',
-  Signature: '#a700e2',
+  Signature: '#fff',
   Legendary: '#ff881a',
   Mystic: 'linear-gradient(130.42deg, #ce9335 0%, #fee5ac 49.12%, #ce9335 98.25%)',
   'One-of-a-kind':
-    'inear-gradient(129.5deg, #2e62f6 0%, #fa9ffb 20%, #7df0cf 40%, #fbfecf 60%, #fcd9f1 80%, #91cdf7 100%',
+    'linear-gradient(129.5deg, #2e62f6 0%, #fa9ffb 20%, #7df0cf 40%, #fbfecf 60%, #fcd9f1 80%, #91cdf7 100%)',
 }
 
 export const getNftColor = (rarity: NFT_RARITY) => mapNftColor[rarity]
 
 export const getNftCss = (rarity: NFT_RARITY): CSSProperties => ({
   background: getNftColor(rarity),
+  color: ['One-of-a-kind', 'Signature', 'Mystic', 'Legendary'].includes(rarity) ? '#000' : '#fff',
 })
